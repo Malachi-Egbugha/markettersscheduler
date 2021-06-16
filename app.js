9544024bf13a251df45e03f33fbdb320524cf555
Malachi-Egbugha <malachi.egbugha3@gmail.com>
2021-06-16 16:07:44 +0100

    adjusttimelast

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const cron = require("node-cron");
 require("dotenv").config();
 const { checktransactions } = require("./controllers/scheduler");
 
-cron.schedule("3 08 * * *", checktransactions, {
+cron.schedule("0 8 * * *", checktransactions, {
   scheduled: true,
 });
